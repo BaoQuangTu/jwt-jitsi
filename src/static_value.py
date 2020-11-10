b12f171e1a57a1200f2daf2fbb20f2895bda6348
baoquangtu <baoquangtu150707@gmail.com>
2020-11-10 08:32:52 +0700

    change sub jwt

--- src/static_value.py
+++ src/static_value.py
@@ -1,6 +1,6 @@
 JWT_SECRET = 'BYADjvPiyd55vlhQwmV7AQ7cG0nPI1ItOTISipay31VubKZMY+VC2Qxcin/W09NkfxpY65wvsE50UzyXtNFWMA=='
 JWT_AUDIENCE = 'jitsi'
 JWT_ISSUER = "jitsi-meet-hyperlogy"
-JWT_SUB = "hyperlogy.jit.si"
+JWT_SUB = "meet-jwt.hyperlogy.com"
 JWT_REQUEST_KEY = "Authorization"
 JWT_EXPIRE_AFTER = 1 * 60 * 1000
